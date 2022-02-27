a29f3def7f5ae5fcc31ff6d0b145252ed61e0f46
ByrdBassPC <byrdbass@gmail.com>
2022-02-27 15:22:47 -0600

    setup all three index.js files in root, routes, and api

--- utils/seed.js
+++ utils/seed.js
@@ -1,6 +1,6 @@
 const connection = require('../config/connection');
 const { User, Thought } = require('../models');
-const emails, thoughts, usernames, friends, reactionBodies = require('./data');
+const emails, thoughtText, usernames, friends, reactionBodies = require('./data');
 
 connection.on('error', (err) => err);
 
@@ -9,7 +9,15 @@ connection.once('open', async () => {
     await User.deleteMany({});
     await Thought.deleteMany({});
 
+    const users = []
+    const thoughts = []
+
     await User.collection.insertMany(usernames, thoughts, friends, emails);
     await Thought.collection.insertMany(reactionBodies)
+
+    console.table(users);
+    console.table(thoughts);
+    console.info('Seeding complete! 🌱');
+    process.exit(0);
 })
 
